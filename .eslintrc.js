5e767adcdc9712ea5ed4f74fa951634d82c0ecd0
William Corry <william@wrought-webdesign.co.uk>
2018-09-23 13:08:30 +0100

    feat(App): render app and add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ['plugin:flowtype/recommended', 'airbnb',  'prettier', 'prettier/react'],
-  plugins: ['flowtype', 'eslint-plugin-env', 'prettier'],
+  extends: ['airbnb', 'plugin:flowtype/recommended', 'prettier', 'prettier/react'],
+  plugins: ['flowtype', 'prettier'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
@@ -16,15 +16,20 @@ module.exports = {
     jest: true
   },
   rules: {
-    "react/prop-types": 0,
-    "jsx-a11y/label-has-for": 0,
-    "no-console": 1
-    'jsx-a11y/href-no-hash': 1,
+    'react/prop-types': 0,
+    'jsx-a11y/label-has-for': 0,
+    'no-console': 1,
+    'jsx-a11y/href-no-hash': 0,
     'jsx-a11y/anchor-is-valid': ['warn', { aspects: ['invalidHref'] }],
     'react/prefer-stateless-function': [1, { ignorePureComponents: true }],
     'react/no-access-state-in-setstate': 0,
     'react/button-has-type': 0,
     'import/prefer-default-export': 0,
     'flowtype/space-after-type-colon': [2, 'always', { allowLineBreak: true }]
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {}
+    }
   }
 };
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// @flow
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return <div>Rendered</div>;
+  }
+}
